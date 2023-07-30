40aefe378df4835b26760536ff52ecc5b62ad868
FlayInAHook <timmae2001@gmail.com>
2023-07-31 00:38:48 +0200

    added builded version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Box, Center, ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'jotai'
